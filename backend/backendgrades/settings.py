3ab465a9f02fa021106cf06541c5fd6645e29726
David <dcmejiaz@geosat.com.co>
2023-09-02 08:31:13 -0500

    configurando docker

--- backend/backendgrades/settings.py
+++ backend/backendgrades/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grades',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'backendgrades.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',  # Este es el nombre del servicio del contenedor PostgreSQL en docker-compose
+        'PORT': '5432',
     }
 }
 
