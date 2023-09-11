76564c90e422b9e71a829c0c6c9e85d4cf2ca61f
David <dcmejiaz@geosat.com.co>
2023-09-11 09:49:29 -0500

    ajustando redux persistor

--- frontend/gradesfront/src/pages/_app.tsx
+++ frontend/gradesfront/src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import Navbar from '../components/navbar';
-import { store } from '../store';
+import { persistor, store } from '../store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <UserProvider>
-        <Navbar />
-        <Component {...pageProps} />
-      </UserProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <UserProvider>
+          <Navbar />
+          <Component {...pageProps} />
+        </UserProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- frontend/gradesfront/src/store.ts
+++ frontend/gradesfront/src/store.ts
@@ -1,16 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import userReducer from "./slices/userSlice";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 import counterReducer from './slices/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    // posts: postsReducer,
-    // comments: commentsReducer,
-    // users: usersReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  // devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch
