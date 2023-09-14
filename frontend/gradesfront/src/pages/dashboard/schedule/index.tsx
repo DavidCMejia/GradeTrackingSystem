42d7dc197b1f1f9491af77ac407b670a0357b2b5
David <dcmejiaz@geosat.com.co>
2023-09-14 11:21:14 -0500

    ajuste de id dinamico schedule

--- frontend/gradesfront/src/pages/dashboard/schedule/index.tsx
+++ frontend/gradesfront/src/pages/dashboard/schedule/index.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react/no-array-index-key */
 import {
   Alert,
   Badge,
   BadgeProps,
+  Button,
   Calendar,
   CalendarProps,
   DatePicker,
@@ -19,7 +22,7 @@ import dayjs from 'dayjs';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
-import { map } from 'lodash';
+import { get, map } from 'lodash';
 
 import type { Dayjs } from 'dayjs';
 import type { Course } from '../../../types';
@@ -38,6 +41,9 @@ const ScheduleClass: NextPage = () => {
   const [openModal, setOpenModal] = useState(false);
   const [duration, setDuration] = useState(0);
   const courseList: Course[] = useSelector(selectCourses);
+  console.log('🚀 ~ courseList:', courseList);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  console.log('🚀 ~ selectedEvent:', selectedEvent);
   // console.log('🚀 ~ setDuration:', duration); // en MINUTOS
 
   const filterCourses = (input: string, option: any) => {
@@ -51,10 +57,10 @@ const ScheduleClass: NextPage = () => {
       date,
       duration,
     };
-    // console.log('🚀 ~ valuesToSubmit:', {
-    //   ...valuesToSubmit,
-    //   date: valuesToSubmit.date.format('YYYY-MM-DD'),
-    // });
+    console.log('🚀 ~ valuesToSubmit:', {
+      ...valuesToSubmit,
+      date: valuesToSubmit.date.format('YYYY-MM-DD'),
+    });
     // try {
     //   const res = await axios.put(`${URL_BACKEND}/api/students/${values.id}/`, values);
     //   if (res && res.status === 200) {
@@ -80,37 +86,47 @@ const ScheduleClass: NextPage = () => {
 
   useEffect(() => {
     if (openModal) {
+      const event = selectedEvent?.content;
+      const findCourseByName = courseList
+        .find(
+          (course: Course) => course.course_name
+            .toLowerCase()
+            .includes(event.toLowerCase()),
+        );
+
       modalForm.setFieldsValue({
         date,
+        course: findCourseByName?.id,
       });
     }
   }, [openModal]);
+
+  const idCurso = 1;
 
   const calendarData = [
     {
       date: '2023-09-08', // Fecha en formato 'YYYY-MM-DD'
       events: [
-        { type: 'warning', content: 'This is warning event.' },
-        { type: 'success', content: 'This is usual event.' },
+        { content: get(courseList, `[${idCurso}].course_name`) },
       ],
     },
     {
       date: '2023-09-10',
       events: [
-        { type: 'warning', content: 'This is warning event.' },
-        { type: 'success', content: 'This is usual event.' },
-        { type: 'error', content: 'This is error event.' },
+        { content: 'This is warning event.' },
+        { content: 'This is usual event.' },
+        { content: 'This is error event.' },
       ],
     },
     {
       date: '2023-09-15',
       events: [
-        { type: 'warning', content: 'This is warning event' },
-        { type: 'success', content: 'This is very long usual event......' },
-        { type: 'error', content: 'This is error event 1.' },
-        { type: 'error', content: 'This is error event 2.' },
-        { type: 'error', content: 'This is error event 3.' },
-        { type: 'error', content: 'This is error event 4.' },
+        { content: 'This is warning event' },
+        { content: 'This is very long usual event......' },
+        { content: 'This is error event 1.' },
+        { content: 'This is error event 2.' },
+        { content: 'This is error event 3.' },
+        { content: 'This is error event 4.' },
       ],
     },
   ];
@@ -118,28 +134,27 @@ const ScheduleClass: NextPage = () => {
   return (
     <>
       <Calendar
+        mode="month"
         value={date}
         onSelect={onSelect}
         cellRender={(current, info) => {
           if (info.type === 'date') {
             const eventData = calendarData.find((item) => item.date === current.format('YYYY-MM-DD'));
             return eventData ? (
-              <ul className="events">
+              <ul className="events" style={{ listStyleType: 'none' }}>
                 {eventData.events.map((item, index) => (
-                  <li key={index}>
-                    <Badge status={item.type as BadgeProps['status']} text={item.content} />
+                  <li
+                    style={{ listStyle: 'none' }}
+                    key={index}
+                    onClick={() => setSelectedEvent(item)} // Establece el evento seleccionado
+                  >
+                    <Badge
+                      status={item.type ? item.type : 'default'} // Usa 'default' si 'type' no está presente
+                      text={item.content}
+                    />
                   </li>
                 ))}
               </ul>
-            ) : null;
-          }
-          if (info.type === 'month') {
-            const eventData = calendarData.find((item) => item.date === current.format('YYYY-MM-DD'));
-            return eventData ? (
-              <div className="notes-month">
-                <section>{eventData.monthData}</section>
-                <span>Backlog number</span>
-              </div>
             ) : null;
           }
           return info.originNode;
@@ -148,8 +163,12 @@ const ScheduleClass: NextPage = () => {
       <Modal
         title="Schedule Class"
         open={openModal}
-        onCancel={() => setOpenModal(false)}
+        onCancel={() => {
+          setOpenModal(false);
+          setSelectedEvent(null);
+        }}
         onOk={modalForm.submit}
+        okText="Schedule"
       >
         <Form
           form={modalForm}
@@ -157,7 +176,7 @@ const ScheduleClass: NextPage = () => {
           wrapperCol={{ span: 12 }}
           onFinish={onFinish}
         >
-          <Item label="Course" name="courses">
+          <Item label="Course" name="course">
             <Select
               showSearch
               mode="multiple"
